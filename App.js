dc38323af5e8e78b3dcdf88620d530b34948f9af
Lucas Moura Pereira <lucasmoura@ioasys.com.br>
2022-05-26 08:52:40 -0300

    especificando estilos globais

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import React from "react";
+
+import {
+  useFonts,
+  NunitoSans_600SemiBold,
+  NunitoSans_800ExtraBold,
+} from "@expo-google-fonts/nunito-sans";
+
 import { ThemeProvider } from "styled-components";
+
+import AppLoading from 'expo-app-loading';
 
 import Routes from "~/routes";
 import { theme } from "~/utils/styles/theme";
 
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <Routes />
-  </ThemeProvider>
-);
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    NunitoSans_600SemiBold,
+    NunitoSans_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  );
+};
 
 export default App;
--- src/utils/styles/theme.js
+++ src/utils/styles/theme.js
@@ -1,4 +1,23 @@
 export const theme = {
-    bg: '#191a24',
-    color: '#fff'
-}
+  colors: {
+    bg: "#E5E5E5",
+    white: "#FEFEFE",
+    black: "#263238",
+    purple1: "#564666",
+    purple2: "#8870A0",
+    grey: "#C6C6C6",
+  },
+  fonts: {
+    family: {
+      medium: "NunitoSans_600SemiBold",
+      bold: "NunitoSans_800ExtraBold",
+    },
+    sizes: {
+      title: "48px",
+      subTitle: "24px",
+      textInfo: "20px",
+      textLabel: "16px",
+      textForm: "14px",
+    },
+  },
+};
